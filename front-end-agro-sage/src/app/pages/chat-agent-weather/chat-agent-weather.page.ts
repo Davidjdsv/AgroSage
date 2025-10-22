2307e0588d52c98824a7fdbf512ac80141304f07
Davidjdsv <sinisterravalenciajhoandavid@gmail.com>
2025-10-22 09:56:24 -0500

    refactor: -Cambiando las rutas en el routes.ts

--- front-end-agro-sage/src/app/pages/chat-agent-weather/chat-agent-weather.page.ts
+++ front-end-agro-sage/src/app/pages/chat-agent-weather/chat-agent-weather.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-chat-agent-weather',
   templateUrl: './chat-agent-weather.page.html',
   styleUrls: ['./chat-agent-weather.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, CommonModule, FormsModule]
 })
 export class ChatAgentWeatherPage implements OnInit {
 
